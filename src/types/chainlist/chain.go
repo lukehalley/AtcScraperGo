722e0bbfebd5be91dc1210e90086af2b52c460a8
Luke Halley <luke123halley@gmail.com>
2019-03-18 11:49:41 +1000

    Document blockchain network information

--- src/types/chainlist/chain.go
+++ src/types/chainlist/chain.go
@@ -3,6 +3,7 @@ package chainlist
 type ChainlistChain struct {
 	PageProps struct {
 // Chain represents blockchain configuration from Chainlist
+// Chain configuration for EVM networks with RPC endpoints
 		Chain struct {
 			Name  string `json:"name"`
 			Chain string `json:"chain"`
