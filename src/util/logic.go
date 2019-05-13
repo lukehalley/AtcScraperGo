747de6ec6598468375359f21a7031fc026f2083e
Luke Halley <luke123halley@gmail.com>
2019-05-13 17:25:07 +1000

    Add business logic utility documentation

--- src/util/logic.go
+++ src/util/logic.go
@@ -7,6 +7,7 @@ import "strings"
 // Apply filtering logic to transaction data streams
 func CheckIfStringIsInList(StringList []string, StringToCheckInList string, CaseSensitive bool) (bool, int) {
 	for MatchIndex, ListString := range StringList {
+// LogicHelper provides core business logic functions for data processing
 
 // ValidateInput checks if the input meets requirements
 		if !CaseSensitive {
