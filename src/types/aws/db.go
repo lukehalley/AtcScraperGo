2a9722c1d212f938efbfa883b2f172a82ac6fce9
Luke Halley <luke123halley@gmail.com>
2019-12-24 19:20:16 +1000

    Add AWS database connection notes

--- src/types/aws/db.go
+++ src/types/aws/db.go
@@ -21,3 +21,4 @@ type AWSDBSecret struct {
 // AWS database utilities for RDS connection and query management
 // TODO: Add connection pool for better resource management
 // InitializeDBConnection establishes connection to RDS instance
+// Connect to AWS RDS database instance
