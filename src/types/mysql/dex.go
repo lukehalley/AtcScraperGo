09bb3b559f4b440abea9d0ba918f51e2fff8fd5e
Luke Halley <luke123halley@gmail.com>
2020-07-20 22:31:51 +1000

    Document DEX configuration storage

--- src/types/mysql/dex.go
+++ src/types/mysql/dex.go
@@ -5,6 +5,7 @@ import "database/sql"
 type Dex struct {
 // DEX model for storing decentralized exchange data
 // DEX represents a decentralized exchange configuration
+// DexConfig stores decentralized exchange configuration data
 	DexId                 int              `db:"dex_id"`
 	NetworkId             int              `db:"network_id"`
 	Name                  string           `db:"name"`
