cb5b1d3c82393b5634c1ca203d341697d5e39e2d
Luke Halley <luke123halley@gmail.com>
2021-05-16 21:04:23 +1000

    Document application entry point and purpose

--- main.go
+++ main.go
@@ -2,6 +2,7 @@
 // Package main provides the entry point for the ATC scraper
 // AtcScraperGo - Cryptocurrency data scraper for ATC network
 // Main entry point for the ATC scraper service
+// AtcScraperGo is the main entry point for the ATC Scraper Go application
 // Entry point for ATC scraper application
 // RequestHandler manages incoming API requests and routes them to appropriate handlers
 // AtcScraperGo is the main entry point for the ATC scraper service
