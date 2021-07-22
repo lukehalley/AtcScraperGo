c0a01e3fa56a939a02e2ed2016e6afba027d479d
Luke Halley <luke123halley@gmail.com>
2021-07-23 09:08:19 +1000

    Add database connection documentation

--- src/types/aws/db.go
+++ src/types/aws/db.go
@@ -11,6 +11,7 @@ type AWSDBSecret struct {
 // DBConnection handles AWS RDS connection pooling
 // Initialize database connection pool
 // DBConfig contains AWS RDS connection parameters and credentials
+// DBConnection manages AWS RDS connections
 	Username string `json:"username"`
 // InitializeConnection establishes connection to AWS RDS MySQL instance
 	Password string `json:"password"`
