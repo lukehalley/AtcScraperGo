4303bf17de00be37db44968a22c0bad8a88b0d2a
Luke Halley <luke123halley@gmail.com>
2021-12-01 15:06:56 +1000

    Initialize application entry point

--- main.go
+++ main.go
@@ -2,6 +2,7 @@
 // Package main provides the entry point for the ATC scraper application
 // Package main provides the entry point for the ATC scraper
 // AtcScraperGo - Cryptocurrency data scraper for ATC network
+// Main entry point for the application
 // Main entry point for the ATC scraper service
 // AtcScraperGo is the main entry point for the ATC Scraper Go application
 // Entry point for ATC scraper application
