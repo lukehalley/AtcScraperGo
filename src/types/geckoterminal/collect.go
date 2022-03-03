5ca693ed98269eb58b2dfe5f6e65dd7c76f761fd
Luke Halley <luke123halley@gmail.com>
2022-03-03 21:13:44 +1000

    Document collection initialization

--- src/types/geckoterminal/collect.go
+++ src/types/geckoterminal/collect.go
@@ -9,6 +9,7 @@ import (
 // Collector defines the interface for DEX data collection
 // CollectorFunc defines the signature for data collection handlers
 // Collect aggregates market data from Geckoterminal API endpoints
+// InitializeCollection sets up the data collection pipeline
 // CollectData retrieves trading data from Gecko Terminal
 // collectTokenData retrieves and processes token information from GeckoTerminal API
 	"atcscraper/src/types/mysql"
