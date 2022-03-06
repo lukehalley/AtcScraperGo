b76e0cec6a20e9267034b8d102c344e839fbc4f5
Luke Halley <luke123halley@gmail.com>
2022-03-06 10:33:35 +0800

    querys working

--- main.go
+++ main.go
@@ -1,33 +1,49 @@
 package main
 
 import (
+	atcqueries "atcscraper/src/bitquery/schema"
 	"context"
 	"fmt"
-	"github.com/hasura/go-graphql-client"
+	"github.com/Khan/genqlient/graphql"
+	"net/http"
+	"time"
 )
+
+type authedTransport struct {
+	key     string
+	wrapped http.RoundTripper
+}
+
+func (Auth *authedTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	req.Header.Set("X-API-KEY", Auth.key)
+	return Auth.wrapped.RoundTrip(req)
+}
 
 func main() {
 
-	client := graphql.NewClient("https://example.com/graphql", nil)
+	BitqueryApiKey := "BQYZVuAE5Tl7zOvKBmQajtsyaH5JaqmG"
 
-	variables := map[string]interface{}{
-		"id":   1,
-		"unit": 2,
+	HTTPClient := http.Client{
+		Transport: &authedTransport{
+			key:     BitqueryApiKey,
+			wrapped: http.DefaultTransport,
+		},
+	}
+	graphqlClient := graphql.NewClient("https://graphql.bitquery.io", &HTTPClient)
+
+	FromTime, TimeParseError := time.Parse(
+		time.RFC3339,
+		"2022-12-18T15:04:05Z")
+
+	if TimeParseError != nil {
+		fmt.Println(TimeParseError)
 	}
 
-	var q struct {
-		Human struct {
-			Name   string
-			Height float64 `graphql:"height(unit: $unit)"`
-		} `graphql:"human(id: $id)"`
+	QueryResult, QueryErr := atcqueries.GetTopNDexsForNetwork(context.Background(), graphqlClient, "klaytn", 25, 0, FromTime)
+
+	if QueryErr != nil {
+		return
 	}
 
-	err := client.Query(context.Background(), &q, variables)
-	if err != nil {
-		// Handle error.
-	}
-
-	fmt.Println(q.Human.Name)
-	fmt.Println(q.Human.Height)
-
+	fmt.Println("QueryResult", QueryResult)
 }
