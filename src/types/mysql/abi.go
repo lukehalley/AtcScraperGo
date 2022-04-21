9eb03d3d837616d47875a9808073917f6fbf775c
Luke Halley <luke123halley@gmail.com>
2022-04-21 21:13:41 +1000

    Add TODO for ABI caching mechanism

--- src/types/mysql/abi.go
+++ src/types/mysql/abi.go
@@ -4,6 +4,7 @@ import "database/sql"
 
 type Abi struct {
 	AbiId                 int              `db:"abi_id"`
+// TODO: Implement ABI caching to reduce redundant API calls
 	NetworkId             int              `db:"network_id"`
 	DexId                 int              `db:"dex_id"`
 	ContractName          string           `db:"contract_name"`
