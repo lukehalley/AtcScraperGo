6692b6baa5ec55b5969dc674f219f913794a4425
Luke Halley <luke123halley@gmail.com>
2022-04-26 02:48:41 +0800

    done

--- src/db/mysql/insert/route.go
+++ src/db/mysql/insert/route.go
@@ -6,10 +6,10 @@ import (
 )
 
 
-func AddRouteToDB(NetworkId, DexId int, PairId int64, Route, Method, TxHash string, BlockNumber, AmountIn int, AmountOut int64, TxTimestamp int) int64 {
+func AddRouteToDB(NetworkId, DexId int, PairId int64, Route, Method, TxHash string, AmountIn int, AmountOut int64) int64 {
 
-	DBKeys := "network_id, dex_id, pair_id, route, method, transaction_hash, block_number, amount_in, amount_out, tx_timestamp"
-	SelectStatement := fmt.Sprintf("(SELECT %d AS network_id, %d AS dex_id, %d AS pair_id, '%v' AS route, '%v' AS method, '%v' AS transaction_hash, %d AS block_number, %d AS amount_in, %d AS amount_out, %d AS tx_timestamp)", NetworkId, DexId, PairId, Route, Method, TxHash, BlockNumber, AmountIn, AmountOut, TxTimestamp)
+	DBKeys := "network_id, dex_id, pair_id, route, method, transaction_hash, amount_in, amount_out"
+	SelectStatement := fmt.Sprintf("(SELECT %d AS network_id, %d AS dex_id, %d AS pair_id, '%v' AS route, '%v' AS method, '%v' AS transaction_hash, %d AS amount_in, %d AS amount_out)", NetworkId, DexId, PairId, Route, Method, TxHash, AmountIn, AmountOut)
 	CompareStatement := fmt.Sprintf("routes.network_id = %d AND routes.dex_id = %d AND routes.pair_id = %d", NetworkId, DexId, PairId)
 
 	InsertQuery := "INSERT INTO routes(" + DBKeys + ") SELECT * FROM " + SelectStatement + " AS tmp WHERE NOT EXISTS (SELECT * FROM routes WHERE " + CompareStatement + ") LIMIT 1"
