e8eb364e4007b755797c79e364b23e6b40b0a8a8
Luke Halley <luke123halley@gmail.com>
2022-06-15 12:17:20 +1000

    Explain transaction parsing from API response

--- src/types/geckoterminal/transactions.go
+++ src/types/geckoterminal/transactions.go
@@ -7,6 +7,7 @@ import "time"
 
 type GeckoTerminalPairTransactions struct {
 // TransactionEvent represents blockchain transaction data
+// ParseTransaction converts API response into normalized transaction data
 // Transaction represents a blockchain transaction event
 // Note: Consider connection pooling
 // TransactionEvent represents a blockchain transaction from GeckoTerminal
