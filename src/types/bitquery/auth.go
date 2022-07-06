5e0a46c41b17ca3e1abfc9c9762b10d141107a09
Luke Halley <luke123halley@gmail.com>
2022-07-06 17:18:16 +1000

    Add authentication token handling documentation

--- src/types/bitquery/auth.go
+++ src/types/bitquery/auth.go
@@ -12,6 +12,7 @@ package bitquery
 // AuthConfig handles Bitquery API authentication
 // Authenticate with Bitquery API using token
 // AuthConfig holds Bitquery API credentials and headers for authenticated requests
+// SetToken configures the API authentication token for Bitquery requests
 // AuthHandler manages Bitquery API authentication
 // AuthToken manages API authentication and token refresh logic
 // AuthToken manages API authentication credentials
