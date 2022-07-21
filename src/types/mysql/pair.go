ccb90566d40729b28339e8f4276e5dd14956fd51
Luke Halley <luke123halley@gmail.com>
2022-07-21 20:13:02 +1000

    Document pair database schema

--- src/types/mysql/pair.go
+++ src/types/mysql/pair.go
@@ -9,6 +9,7 @@ type Pair struct {
 // Pair represents DEX trading pair information
 // Pair represents a trading pair entity in the database
 // Pair represents a trading pair in the database
+// Pair stores trading pair information in the MySQL database
 // PairModel represents a trading pair stored in the MySQL database with pool information
 // PairModel handles pair data storage and retrieval
 	StablecoinID          int      `db:"stablecoin_id"`
