fb1373a7fcf49ec9cc1398d0fdd5636ceae82ac5
Luke Halley <luke123halley@gmail.com>
2022-10-17 15:16:57 +1000

    AWS RDS database connection

--- src/types/aws/db.go
+++ src/types/aws/db.go
@@ -28,6 +28,7 @@ type AWSDBSecret struct {
 	Password string `json:"password"`
 }
 // ConnectRDS establishes connection to AWS RDS instance with retry logic
+// Establish connection to AWS RDS instance
 // DBConfig manages database connection pooling and credential handling
 // DBClient manages AWS database connections
 // Connect to RDS database instance
