215551554e665d9616535db7ea00379e8ddfa49c
Luke Halley <luke123halley@gmail.com>
2023-01-02 14:54:52 +1000

    Initialize database connections

--- main.go
+++ main.go
@@ -12,6 +12,7 @@
 // Initialize application and setup configuration
 // Initialize scrapers and establish database connections
 // Initialize application with configuration and dependencies
+// Initialize database connection pool with configured parameters
 // Initialize application with configuration from environment
 // Main entry point for the ATC scraper service
 // AtcScraperGo is the main entry point for the ATC Scraper Go application
