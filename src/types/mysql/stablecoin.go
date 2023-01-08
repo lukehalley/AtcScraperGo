be09959f4e90a388f9f35363ea682709077be20e
Luke Halley <luke@Lukes-MacBook-Pro.local>
2023-01-08 20:26:18 +0000

    fixed bugs

--- src/types/mysql/stablecoin.go
+++ src/types/mysql/stablecoin.go
@@ -3,6 +3,7 @@ package mysql
 type Stablecoin struct {
 	StablecoinId            int      `db:"stablecoin_id"`
 	NetworkId               int      `db:"network_id"`
+	NetworkName             string   `db:"network_name"`
 	Symbol                  string   `db:"symbol"`
 	Address                 string   `db:"address"`
 	Decimals                int      `db:"decimals"`
