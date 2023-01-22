2d2d6c2c4f30407f8d254674b87bf5afe97dd8f4
Luke Halley <luke123halley@gmail.com>
2023-01-22 17:17:02 +0000

    removed dex valid check

--- src/routines/pairs.go
+++ src/routines/pairs.go
@@ -5,7 +5,6 @@ import (
 	mysql_insert "atcscraper/src/db/mysql/insert"
 	mysql_query "atcscraper/src/db/mysql/query"
 	geckoterminal_types "atcscraper/src/types/geckoterminal"
-	"atcscraper/src/util"
 	"sync"
 )
 
@@ -15,74 +14,73 @@ func CollectGeckoTerminalDexPairs(Network geckoterminal_types.GeckoTerminalNetwo
 	defer DexCollectionWaitGroup.Done()
 
 	// Dex Is Invalid Dex List
-	DexIsInvalid, _ := util.CheckIfStringIsInList(InvalidDexs, Dex.Identifier, false)
+	// DexIsInvalid, _ := util.CheckIfStringIsInList(InvalidDexs, Dex.Identifier, false)
+	//if !DexIsInvalid {
+	//
+	//}
 
-	if !DexIsInvalid {
+	// Check If Dex Is Already Stored
+	DexQueryResults := mysql_query.GetDexFromDB(Network.NetworkDBId, Dex.Identifier)
 
-		// Check If Dex Is Already Stored
-		DexQueryResults := mysql_query.GetDexFromDB(Network.NetworkDBId, Dex.Identifier)
+	// Get Dex DB ID
+	DexDBId := 0
+	if len(DexQueryResults) > 0 {
 
-		// Get Dex DB ID
-		DexDBId := 0
-		if len(DexQueryResults) > 0 {
+		// Set Dex DB ID
+		DexDBId = DexQueryResults[0].DexId
 
-			// Set Dex DB ID
-			DexDBId = DexQueryResults[0].DexId
+	} else {
 
-		} else {
+		// Add Dex To DB
+		DexDBId = int(mysql_insert.AddDexToDB(
+			Network.NetworkDBId,
+			Dex.Identifier,
+			Dex.RouterAddress,
+			Dex.FactoryAddress,
+			1,
+		))
 
-			// Add Dex To DB
-			DexDBId = int(mysql_insert.AddDexToDB(
-				Network.NetworkDBId,
-				Dex.Identifier,
-				Dex.RouterAddress,
-				Dex.FactoryAddress,
-				1,
-			))
+	}
 
-		}
+	// Get First Page Dexs
+	DexPairs := geckoterminal_api.GetGeckoterminalDexPairs(Network.Network.Identifier, Dex.Identifier, 1)
 
-		// Get First Page Dexs
-		DexPairs := geckoterminal_api.GetGeckoterminalDexPairs(Network.Network.Identifier, Dex.Identifier, 1)
+	// If We Want To Collect More Than One Page
+	if PagesToCollect > 1 {
 
-		// If We Want To Collect More Than One Page
-		if PagesToCollect > 1 {
+		for i := 2; i <= PagesToCollect; i++ {
 
-			for i := 2; i <= PagesToCollect; i++ {
+			// Get All Pairs For Current Dex
+			CurrentPageDexPairs := geckoterminal_api.GetGeckoterminalDexPairs(Network.Network.Identifier, Dex.Identifier, i)
 
-				// Get All Pairs For Current Dex
-				CurrentPageDexPairs := geckoterminal_api.GetGeckoterminalDexPairs(Network.Network.Identifier, Dex.Identifier, i)
-
-				// Add The Rest Of The Pairs To The List
-				if len(CurrentPageDexPairs.Data) > 0 {
-					for _, CurrentPageDexPair := range CurrentPageDexPairs.Data {
-						DexPairs.Data = append(DexPairs.Data, CurrentPageDexPair)
-					}
-				} else {
-					break
+			// Add The Rest Of The Pairs To The List
+			if len(CurrentPageDexPairs.Data) > 0 {
+				for _, CurrentPageDexPair := range CurrentPageDexPairs.Data {
+					DexPairs.Data = append(DexPairs.Data, CurrentPageDexPair)
 				}
-
+			} else {
+				break
 			}
 
 		}
 
-		// Create Concurrency Objects
-		PairCollectionWaitGroup := new(sync.WaitGroup)
-		PairCollectionWaitGroup.Add(len(DexPairs.Data))
-		PairCollectionChannel := make(chan geckoterminal_types.Pair, len(DexPairs.Data))
-
-		// Iterate Through Networks Dexs And Get All Their Pairs
-		for _, DexPair := range DexPairs.Data {
-			go ScrapePairInfo(Network, NetworkStablecoins, Dex, DexDBId, DexPair, BlacklistPairAddresses, TxsToCollect, PairCollectionWaitGroup, PairCollectionChannel)
-		}
-
-		// Wait For All Networks To Come Back
-		PairCollectionWaitGroup.Wait()
-
-		// Close The Group Channel
-		close(PairCollectionChannel)
-
 	}
+
+	// Create Concurrency Objects
+	PairCollectionWaitGroup := new(sync.WaitGroup)
+	PairCollectionWaitGroup.Add(len(DexPairs.Data))
+	PairCollectionChannel := make(chan geckoterminal_types.Pair, len(DexPairs.Data))
+
+	// Iterate Through Networks Dexs And Get All Their Pairs
+	for _, DexPair := range DexPairs.Data {
+		go ScrapePairInfo(Network, NetworkStablecoins, Dex, DexDBId, DexPair, BlacklistPairAddresses, TxsToCollect, PairCollectionWaitGroup, PairCollectionChannel)
+	}
+
+	// Wait For All Networks To Come Back
+	PairCollectionWaitGroup.Wait()
+
+	// Close The Group Channel
+	close(PairCollectionChannel)
 
 	DexCollectionChannel <- Dex
 	
