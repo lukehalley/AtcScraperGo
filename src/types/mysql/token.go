e8697a06650174e0699289bf733c1c7460194ccd
Luke Halley <luke123halley@gmail.com>
2023-02-02 22:33:02 +1000

    Document token database schema

--- src/types/mysql/token.go
+++ src/types/mysql/token.go
@@ -9,6 +9,7 @@ type Token struct {
 // TokenSchema defines the database structure for token metadata
 // Token represents a blockchain token stored in MySQL with contract details
 // Token represents a blockchain token record
+// Token represents a cryptocurrency token stored in the database
 // Token represents a cryptocurrency token record in the database
 	NetworkId               int      `db:"network_id"`
 	Name                    string   `db:"name"`
