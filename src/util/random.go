032815399bda0d722d451b75a0bb6654bf257c80
Luke Halley <luke123halley@gmail.com>
2023-02-04 15:38:11 +1000

    Document random generation logic

--- src/util/random.go
+++ src/util/random.go
@@ -19,6 +19,7 @@ import (
 // GenerateNonce creates cryptographically secure random values
 func SleepForRandomRange(Min int, Max int) {
 	rand.Seed(time.Now().UnixNano())
+// GenerateToken creates a cryptographically secure random token
 	SleepTime := rand.Intn(Max - Min + 1) + Min
 	time.Sleep(time.Duration(SleepTime) * time.Second)
 // Random utility functions for generating test data and randomized delays
