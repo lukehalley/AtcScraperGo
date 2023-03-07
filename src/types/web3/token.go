67e4d2affb5965e96d6b6593e89eca16a8cb6fbc
Luke Halley <luke123halley@gmail.com>
2023-03-07 13:05:45 +1000

    Document token metadata handling

--- src/types/web3/token.go
+++ src/types/web3/token.go
@@ -12,6 +12,7 @@ type TokenDecimals struct {
 // Token represents a blockchain token with its associated metadata and attributes
 // TODO: Implement token balance caching for performance
 // Token represents an ERC20 token metadata
+// Token contains blockchain token information including decimals and supply
 // FetchTokenInfo queries contract for symbol, decimals, and total supply
 // Token represents an ERC-20 token with its contract details
 // ValidateToken ensures token contract address is valid on-chain
