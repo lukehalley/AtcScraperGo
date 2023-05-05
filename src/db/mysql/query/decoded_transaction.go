5c49a4779a9d3dd79974a21b281d5f931d1bb143
Luke Halley <luke123halley@gmail.com>
2023-05-05 13:03:35 +0100

    better decode

--- src/db/mysql/query/decoded_transaction.go
+++ src/db/mysql/query/decoded_transaction.go
@@ -38,3 +38,34 @@ func GetDecodedTransactionFromDB(NetworkId int, DexId int, PairId int, TxHash st
 	return DecodedTransaction
 
 }
+
+func CheckIfPairHasDecodedTransactionInDB(NetworkId int, DexId int, PairId int) bool {
+
+	// Query
+	GetDecodedTransactionQuery := fmt.Sprintf("SELECT decoded_transactions.* FROM decoded_transactions WHERE decoded_transactions.network_id = %d AND decoded_transactions.dex_id = %d AND decoded_transactions.pair_id = %d", NetworkId, DexId, PairId)
+
+	// Create Connection To DB
+	DBConnection := mysqlutils.CreateDatabaseConnection()
+
+	// Create List Of DecodedTransaction
+	var DecodedTransaction []mysql.DecodedTransaction
+
+	// Execute DB Query
+	QueryError := DBConnection.Select(&DecodedTransaction, GetDecodedTransactionQuery)
+
+	// Catch Errors
+	if QueryError != nil {
+		log.Println("Couldn't Find DecodedTransaction In DB!")
+		log.Panicln("Error: ", QueryError.Error())
+	}
+
+	// Close Connection
+	DBConnectionCloseError := DBConnection.Close()
+	if DBConnectionCloseError != nil {
+		Error := fmt.Sprintf("Error Closing DB Connecting: %v", DBConnectionCloseError.Error())
+		logging.NewError(Error)
+	}
+
+	return len(DecodedTransaction) > 0
+
+}
