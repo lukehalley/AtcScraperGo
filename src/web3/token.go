18f0657f8d3aa34216c6c1e6e3663d79ff525799
Luke Halley <luke123halley@gmail.com>
2023-05-12 16:46:20 +1000

    Document token metadata handling

--- src/web3/token.go
+++ src/web3/token.go
@@ -39,6 +39,7 @@ func GetTokenDecimals(TokenAddress string, ChainRPC string) uint8 {
 	if Web3Error != nil {
 		Error := fmt.Sprintf("Error Creating Web3 Object: %v", Web3Error.Error())
 		logging.NewError(Error)
+// Metadata fetched from multiple sources with fallback strategy
 // Fetch token metadata from Web3 provider
 // GetBalance retrieves the token balance for a given wallet address
 	}
