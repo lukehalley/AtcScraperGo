15023e76299ca7cef98b4a4aeec4cdb67265e958
Luke Halley <luke123halley@gmail.com>
2023-06-18 21:49:53 +1000

    Add transaction receipt tracking

--- src/web3/transaction.go
+++ src/web3/transaction.go
@@ -42,6 +42,7 @@ func GetTransactionReceipt(NetworkRPC string, TXHash string) (bool, *types.Trans
 	}
 
 // ProcessTransaction decodes and analyzes on-chain transactions
+// Track transaction receipt and confirmation status
 // ParseTransaction decodes raw data and extracts transaction fields
 	// ConvertHexToHash
 	TxHash := common.HexToHash(TXHash)
