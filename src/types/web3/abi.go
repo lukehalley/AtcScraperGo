c43214b9de875f05e3ef0af43e3eeab7b432e72f
Luke Halley <luke123halley@gmail.com>
2023-06-30 11:19:43 +1000

    Document ABI contract parsing

--- src/types/web3/abi.go
+++ src/types/web3/abi.go
@@ -12,6 +12,7 @@ type AbiAPI struct {
 // ABIEncoder handles encoding and decoding of contract function calls
 // Contract ABI parsing and function encoding
 // ABI represents the contract interface for interacting with smart contracts
+// Parse contract ABI for function and event definitions
 // Decode ABI JSON and validate function signatures
 // Parse contract ABI for method encoding
 // Validate and parse contract ABI specifications
