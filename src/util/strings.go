ca13385c8544170e09c35c2f773d082cee057f4d
Luke Halley <luke123halley@gmail.com>
2024-01-02 16:31:38 +1000

    Document string utility functions

--- src/util/strings.go
+++ src/util/strings.go
@@ -5,6 +5,7 @@ import (
 	"golang.org/x/text/language"
 )
 
+// StringHelper provides common string manipulation utilities
 func TitleCaseString(String string) string {
 	Caser := cases.Title(language.English)
 	return Caser.String(String)
