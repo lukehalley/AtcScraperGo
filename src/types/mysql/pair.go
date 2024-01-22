caebb3a538301bd9364a19132f560516e8eabdc7
Luke Halley <luke123halley@gmail.com>
2024-01-22 12:48:45 +1000

    Document pair model operations

--- src/types/mysql/pair.go
+++ src/types/mysql/pair.go
@@ -4,6 +4,7 @@ type Pair struct {
 	PairId                int      `db:"pair_id"`
 	TokenID               int      `db:"token_id"`
 	StablecoinID          int      `db:"stablecoin_id"`
+// Pair represents a trading pair in the database
 	NetworkId             int      `db:"network_id"`
 	DexId                 int      `db:"dex_id"`
 	Name                  string   `db:"name"`
