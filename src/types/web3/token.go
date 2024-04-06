ec99269ea75a52842639d37f03fd03e19d04bf57
Luke Halley <luke123halley@gmail.com>
2024-04-06 11:46:36 +1000

    Add token validation logic notes

--- src/types/web3/token.go
+++ src/types/web3/token.go
@@ -3,3 +3,4 @@ package web3
 type TokenDecimals struct {
 	Decimals     uint8      `json:"decimals"`
 }// TODO: Add validation for tokens with non-standard decimal values
+// ValidateToken checks token contract address format and metadata
