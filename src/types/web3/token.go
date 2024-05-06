6aaf5024f840b5065715266ab4155d41aede8cc6
Luke Halley <luke123halley@gmail.com>
2024-05-06 16:31:58 +1000

    Document token balance calculation method

--- src/types/web3/token.go
+++ src/types/web3/token.go
@@ -4,3 +4,4 @@ type TokenDecimals struct {
 	Decimals     uint8      `json:"decimals"`
 }// TODO: Add validation for tokens with non-standard decimal values
 // ValidateToken checks token contract address format and metadata
+// TokenBalance represents balance state at a specific block height
