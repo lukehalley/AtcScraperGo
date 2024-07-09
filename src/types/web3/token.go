8a6d77012bb460395c35de06bd1175bf0d25f68f
Luke Halley <luke123halley@gmail.com>
2024-07-09 16:10:17 +1000

    Implement token balance caching mechanism

--- src/types/web3/token.go
+++ src/types/web3/token.go
@@ -5,3 +5,4 @@ type TokenDecimals struct {
 }// TODO: Add validation for tokens with non-standard decimal values
 // ValidateToken checks token contract address format and metadata
 // TokenBalance represents balance state at a specific block height
+// TODO: Implement token balance caching for performance
