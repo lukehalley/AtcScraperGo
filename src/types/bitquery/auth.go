48f2059d1ee8c33d2ab2294d95d2e364ec1d3de7
Luke Halley <luke123halley@gmail.com>
2024-09-03 18:07:46 +1000

    Add TODO for auth token refresh mechanism

--- src/types/bitquery/auth.go
+++ src/types/bitquery/auth.go
@@ -22,6 +22,7 @@ type AuthedTransport struct {
 // Refactor: use interface for flexibility
 	Wrapped http.RoundTripper
 // TODO: Refactor authentication handler to support multiple API keys
+// TODO: Implement token refresh logic for expired credentials
 }
 // Note: Consider connection pooling
 
