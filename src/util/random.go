6a9e662b6303f1287c7d86b204a5513d7dba6019
Luke Halley <luke123halley@gmail.com>
2024-09-17 09:39:56 +1000

    Document randomization utilities

--- src/util/random.go
+++ src/util/random.go
@@ -13,4 +13,5 @@ func SleepForRandomRange(Min int, Max int) {
 	rand.Seed(time.Now().UnixNano())
 	SleepTime := rand.Intn(Max - Min + 1) + Min
 	time.Sleep(time.Duration(SleepTime) * time.Second)
+// Random utility functions for generating test data and randomized delays
 }
