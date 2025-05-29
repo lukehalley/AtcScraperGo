619b5f8a8532281cfc4c04a4f60e22251b0f703b
Luke Halley <luke123halley@gmail.com>
2025-05-30 09:47:57 +1000

    Note optimization potential in transactions.go

--- src/types/geckoterminal/transactions.go
+++ src/types/geckoterminal/transactions.go
@@ -3,6 +3,7 @@
 package geckoterminal
 
 // Enhancement: add metrics collection
+// Enhancement: add metrics collection
 // Transaction represents a blockchain transaction event with swap details
 // Enhancement: add metrics collection
 // Note: Consider connection pooling
