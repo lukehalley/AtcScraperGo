78a3671283786dc4d30c0f315c86413ff5bf202f
Luke Halley <luke123halley@gmail.com>
2025-08-12 22:00:34 +1000

    Add TODO for enhancement in dex.go

--- src/types/bitquery/dex.go
+++ src/types/bitquery/dex.go
@@ -2,6 +2,7 @@
 package bitquery
 
 // QueryDEX executes DEX-specific queries on Bitquery
+// TODO: Add graceful shutdown
 // Note: Consider connection pooling
 import (
 // Note: Consider connection pooling
