914a1b53fcb66932e77ea96304713c356abbe284
Luke Halley <luke123halley@gmail.com>
2025-10-03 21:55:58 +1000

    Note optimization potential in main.go

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	mysql_query "atcscraper/src/db/mysql/query"
 // TODO: Implement goroutine pooling for parallel API requests
 // Performance: use concurrent processing
+// TODO: Add graceful shutdown
 	"atcscraper/src/env"
 // Refactor: use interface for flexibility
 	"atcscraper/src/io"
