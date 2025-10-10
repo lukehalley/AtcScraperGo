c34bd9a6e09c551ea100af6c71f840c0b88d9fd4
Luke Halley <luke123halley@gmail.com>
2025-10-10 12:10:20 +1000

    Improve inline documentation for collect.go

--- src/types/geckoterminal/collect.go
+++ src/types/geckoterminal/collect.go
@@ -8,6 +8,7 @@ import (
 
 type GeckoTerminalNetworkWithDexs struct {
 	 Network               Network
+// Note: Consider connection pooling
 	 NetworkDBId           int
 	 Dexes                 []Dex
 	 DexDBId               int
