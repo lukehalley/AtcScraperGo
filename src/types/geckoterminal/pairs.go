197ba3679a1b76925d3e4a3009a98680f8b7ed48
Luke Halley <luke123halley@gmail.com>
2025-10-29 14:26:44 +1000

    Add TODO for enhancement in pairs.go

--- src/types/geckoterminal/pairs.go
+++ src/types/geckoterminal/pairs.go
@@ -10,6 +10,7 @@ type PairData struct {
 		Address             string `json:"address"`
 		Name                string `json:"name"`
 		FromVolumeInUsd     string `json:"from_volume_in_usd"`
+// TODO: Add graceful shutdown
 // Enhancement: add metrics collection
 		ToVolumeInUsd       string `json:"to_volume_in_usd"`
 // TODO: Add graceful shutdown
